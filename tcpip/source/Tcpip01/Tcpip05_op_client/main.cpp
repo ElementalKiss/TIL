03b5b958625cecb55b0a814242bf6d8e05d36f4e
elkiss <jominhyuk87@gmail.com>
2019-02-27 02:19:26 +0900

    Added ch 05 project.

---
+++ tcpip/source/Tcpip01/Tcpip05_op_client/main.cpp
@@ -0,0 +1,77 @@
+﻿// Tcpip05_op_client.cpp: 콘솔 ?�용 ?�로그램??진입?�을 ?�의?�니??
+//
+
+#include "stdafx.h"
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <string>
+#include <WS2tcpip.h>
+
+#define BUF_SIZE 1024
+#define RLT_SIZE 4
+#define OPSZ 4
+void ErrorHandling(const std::string message);
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsaData;
+    SOCKET hSocket;
+    char opmsg[BUF_SIZE];
+    int result, opndCnt, i;
+    SOCKADDR_IN servAdr;
+
+    if (argc != 3) {
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
+    
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        ErrorHandling("WSAStartup() error!"); 
+    }
+
+    hSocket = socket(PF_INET, SOCK_STREAM, 0);   
+    if (hSocket == INVALID_SOCKET) {
+        ErrorHandling("socket() error");
+    }
+
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = inet_addr(argv[1]);
+	servAdr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(hSocket, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR) {
+        ErrorHandling("connect() error!");
+    } else {
+        puts("Connected...........");
+    }
+
+    fputs("Operand count: ", stdout);
+    scanf("%d", &opndCnt);
+    opmsg[0] = (char)opndCnt;
+
+    for (i = 0; i < opndCnt; i++) {
+        printf("Operand %d: ", i + 1);
+        scanf("%d", (int*)&opmsg[i * OPSZ + 1]);
+    }
+
+    fgetc(stdin);
+    fputs("Operator: ", stdout);
+    scanf("%c", &opmsg[opndCnt * OPSZ + 1]);
+    send(hSocket, opmsg, opndCnt * OPSZ + 2, 0);
+    recv(hSocket, (char*)&result, RLT_SIZE, 0);
+
+    printf("Operation result: %d \n", result);
+    closesocket(hSocket);
+    WSACleanup();
+    return 0;
+}
+
+void ErrorHandling(const std::string message)
+{
+    fputs(message.c_str(), stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
---
+++ tcpip/source/Tcpip01/Tcpip05_op_server/main.cpp
@@ -0,0 +1,113 @@
+﻿// Tcpip05_op_server.cpp: 콘솔 ?�용 ?�로그램??진입?�을 ?�의?�니??
+//
+
+#include "stdafx.h"
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <string>
+#include <WS2tcpip.h>
+
+#define BUF_SIZE 1024
+#define OPSZ 4
+
+int Calculate(int opnum, int opnds[], char op);
+void ErrorHandling(const std::string message);
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsaData;
+    SOCKET hServSock;
+    SOCKET hClntSock;
+    
+    if (argc != 2) {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        ErrorHandling("WSAStartup() error!"); 
+    }
+
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);   
+    if (hServSock == INVALID_SOCKET) {
+        ErrorHandling("socket() error");
+    }
+
+    SOCKADDR_IN servAdr, clntAdr;
+    memset(&servAdr, 0, sizeof(servAdr));
+    servAdr.sin_family = AF_INET;
+    servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAdr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR) {
+        ErrorHandling("bind() error");
+    }
+
+    if (listen(hServSock, 5) == SOCKET_ERROR) {
+        ErrorHandling("listen() error");	
+    }
+    
+    int clntAdrSize = sizeof(clntAdr);
+
+    int result;
+    int opndCnt;
+    int recvCnt;
+    int recvLen;
+    char opinfo[BUF_SIZE];
+
+    for (int i = 0; i < 5; i++) {
+        opndCnt = 0;
+        hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &clntAdrSize);	
+        int iResult = recv(hClntSock, (char*)(&opndCnt), 1, 0);
+        if (iResult > 0) {
+            printf("Bytes received: %d\n", iResult);
+        } else if (iResult == 0) {
+            printf("Connection closing...\n");
+        } else {
+            printf("recv failed with error close socket: %d\n", WSAGetLastError());
+            closesocket(hClntSock);
+        }
+
+        recvLen = 0;
+        while ((opndCnt * OPSZ + 1) > recvLen) {
+            recvCnt = recv(hClntSock, &opinfo[recvLen], BUF_SIZE - 1, 0);
+            recvLen += recvCnt;
+        }
+
+        result = Calculate(opndCnt, (int*)opinfo, opinfo[recvLen - 1]);
+        printf("Caclulated num: %d close socket\n", result);
+        send(hClntSock, (char*)&result, sizeof(result), 0);
+        closesocket(hClntSock);
+    }
+
+    closesocket(hServSock);
+    WSACleanup();
+    return 0;
+}
+
+int Calculate(int opnum, int opnds[], char op)
+{
+    int result = opnds[0];
+
+    switch(op) {
+    case '+':
+        for(int i = 1; i<opnum; i++) result += opnds[i];
+        break;
+    case '-':
+        for(int i = 1; i<opnum; i++) result -= opnds[i];
+        break;
+    case '*':
+        for(int i = 1; i<opnum; i++) result *= opnds[i];
+        break;
+    }
+    return result;
+}
+
+void ErrorHandling(const std::string message)
+{
+    fputs(message.c_str(), stderr);
+    fputc('\n', stderr);
+    exit(1);
+}

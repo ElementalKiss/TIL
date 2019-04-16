c4f6627a1f994ac8720f10390fbf1d2aa5a0d41e
elkiss <jominhyuk87@gmail.com>
2019-04-16 23:41:05 +0900

    Updated ch10 md file. Added zombie, wait cpp source.

---
+++ tcpip/source/Linux/Tcpip10/wait.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[])
+{
+    int status;
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        return 3;
+    } else {
+        print("child pid: %d \n", pid);
+        pid = fork();
+        if (pid == 0) {
+            exit(7);
+        } else {
+            printf("child pid: %d \n", pid);
+            wait(&status);
+            if (WIFEXITED(status)) {
+                printf("child send one: %d \n", WEXITSTATUS(status));
+            }
+
+            wait(&status);
+            if (WIFEXITED(status)) {
+                printf("child send two: %d \n", WEXITSTATUS(status));
+            }
+
+            sleep(30);
+        }
+    }
+
+    return 0;
+}
---
+++ tcpip/source/Linux/Tcpip10/zombie.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    pid_t pid = fork();
+
+    // if child process
+    if (pid == 0) {
+        puts("Hi, I am a child process");
+    } else {
+        printf("clie process ID: %d \n", pid);
+        sleep(30);
+    }
+
+    if (pid == 0) {
+        puts("End child process");
+    } else {
+        puts("End parent process");
+    }
+    
+    return 0;
+}

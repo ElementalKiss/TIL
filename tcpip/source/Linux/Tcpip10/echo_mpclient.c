53ce877fe797c9d39a869612755c45f0a20111de
elkiss <jominhyuk87@gmail.com>
2019-04-23 23:18:09 +0900

    remove tab

--- tcpip/source/Linux/Tcpip10/echo_mpclient.c
+++ tcpip/source/Linux/Tcpip10/echo_mpclient.c
@@ -12,66 +12,66 @@ void write_routine(int sock, char* buf);
 
 int main(int argc, char* argv[])
 {
-	int sock;
-	pid_t pid;
-	char buf[BUF_SIZE];
-	struct sockaddr_in serv_adr;
-	
+    int sock;
+    pid_t pid;
+    char buf[BUF_SIZE];
+    struct sockaddr_in serv_adr;
+
     if (argc != 3) {
-		printf("Usage : %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
-	
-	sock = socket(PF_INET, SOCK_STREAM, 0);  
-	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_adr.sin_port = htons(atoi(argv[2]));
-	
-	if (connect(sock, (struct sockaddr* )&serv_adr, sizeof(serv_adr)) == -1) {
-		error_handling("connect() error!");
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
     }
 
-	pid = fork();
+    sock = socket(PF_INET, SOCK_STREAM, 0);  
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
 
-	if (pid == 0) {
-		write_routine(sock, buf);
+    if (connect(sock, (struct sockaddr* )&serv_adr, sizeof(serv_adr)) == -1) {
+        error_handling("connect() error!");
+    }
+
+    pid = fork();
+
+    if (pid == 0) {
+        write_routine(sock, buf);
     } else {
-		read_routine(sock, buf);
+        read_routine(sock, buf);
     }
 
-	close(sock);
-	return 0;
+    close(sock);
+    return 0;
 }
 
 void read_routine(int sock, char* buf)
 {
-	while (1) {
-		int str_len = read(sock, buf, BUF_SIZE);
-		if (str_len == 0) {
-			return;
+    while (1) {
+        int str_len = read(sock, buf, BUF_SIZE);
+        if (str_len == 0) {
+            return;
         }
 
-		buf[str_len] = 0;
-		printf("Message from server: %s", buf);
-	}
+        buf[str_len] = 0;
+        printf("Message from server: %s", buf);
+    }
 }
 
 void write_routine(int sock, char* buf)
 {
-	while (1) {
-		fgets(buf, BUF_SIZE, stdin);
-		if (!strcmp(buf,"q\n") || !strcmp(buf,"Q\n")) {	
-			shutdown(sock, SHUT_WR);
-			return;
-		}
+    while (1) {
+        fgets(buf, BUF_SIZE, stdin);
+        if (!strcmp(buf,"q\n") || !strcmp(buf,"Q\n")) {
+            shutdown(sock, SHUT_WR);
+            return;
+        }
 
-		write(sock, buf, strlen(buf));
-	}
+        write(sock, buf, strlen(buf));
+    }
 }
 void error_handling(char* message)
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }

7143164e7e0c15120fbd5465ca91b0b349aa98c5
elkiss <jominhyuk87@gmail.com>
2019-06-04 21:52:00 +0900

    Added ch14 source files

--- tcpip/source/Linux/Tcpip13/oob_recv.c
+++ tcpip/source/Linux/Tcpip13/oob_recv.c
@@ -16,66 +16,66 @@ int recv_sock;
 
 int main(int argc, char* argv[])
 {
-	struct sockaddr_in recv_adr, serv_adr;
-	int str_len, state;
-	socklen_t serv_adr_sz;
-	struct sigaction act;
-	char buf[BUF_SIZE];
+    struct sockaddr_in recv_adr, serv_adr;
+    int str_len, state;
+    socklen_t serv_adr_sz;
+    struct sigaction act;
+    char buf[BUF_SIZE];
 
-	if (argc!=2) {
-		printf("Usage : %s <port>\n", argv[0]); 
-		exit(1);	
-	 }
-	
-	act.sa_handler = urg_handler;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = 0; 
-	
-	acpt_sock = socket(PF_INET, SOCK_STREAM, 0);
-	memset(&recv_adr, 0, sizeof(recv_adr));
-	recv_adr.sin_family = AF_INET;
-	recv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	recv_adr.sin_port = htons(atoi(argv[1]));
-
-	if (bind(acpt_sock, (struct sockaddr* )&recv_adr, sizeof(recv_adr)) == -1) {
-		error_handling("bind() error");
-    }
-
-	listen(acpt_sock, 5);
-
-	serv_adr_sz = sizeof(serv_adr);
-	recv_sock = accept(acpt_sock, (struct sockaddr* )&serv_adr, &serv_adr_sz);
-	
-	fcntl(recv_sock, F_SETOWN, getpid()); 
-	state = sigaction(SIGURG, &act, 0);
-	
-	while ((str_len = recv(recv_sock, buf, sizeof(buf), 0)) != 0)  {
-		if (str_len == -1) {
-			continue;
+    if (argc!=2) {
+        printf("Usage : %s <port>\n", argv[0]); 
+        exit(1);	
         }
 
-		buf[str_len] = 0;
-		puts(buf);
-	}
+    act.sa_handler = urg_handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0; 
 
-	close(recv_sock);
-	close(acpt_sock);
-	return 0; 
+    acpt_sock = socket(PF_INET, SOCK_STREAM, 0);
+    memset(&recv_adr, 0, sizeof(recv_adr));
+    recv_adr.sin_family = AF_INET;
+    recv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    recv_adr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(acpt_sock, (struct sockaddr* )&recv_adr, sizeof(recv_adr)) == -1) {
+        error_handling("bind() error");
+    }
+
+    listen(acpt_sock, 5);
+
+    serv_adr_sz = sizeof(serv_adr);
+    recv_sock = accept(acpt_sock, (struct sockaddr* )&serv_adr, &serv_adr_sz);
+
+    fcntl(recv_sock, F_SETOWN, getpid()); 
+    state = sigaction(SIGURG, &act, 0);
+
+    while ((str_len = recv(recv_sock, buf, sizeof(buf), 0)) != 0)  {
+        if (str_len == -1) {
+            continue;
+        }
+
+        buf[str_len] = 0;
+        puts(buf);
+    }
+
+    close(recv_sock);
+    close(acpt_sock);
+    return 0; 
 }
 
 void urg_handler(int signo)
 {
-	int str_len;
-	char buf[BUF_SIZE];
+    int str_len;
+    char buf[BUF_SIZE];
 
-	str_len = recv(recv_sock, buf, sizeof(buf) - 1, MSG_OOB);
-	buf[str_len] = 0;
-	printf("Urgent message: %s \n", buf);
+    str_len = recv(recv_sock, buf, sizeof(buf) - 1, MSG_OOB);
+    buf[str_len] = 0;
+    printf("Urgent message: %s \n", buf);
 }
 
 void error_handling(char* message)
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
--- tcpip/source/Linux/Tcpip13/readv.c
+++ tcpip/source/Linux/Tcpip13/readv.c
@@ -19,5 +19,5 @@ int main(int argc, char* argv[])
     printf("First message: %s \n", buf1);
     printf("Second message: %s \n", buf2);
 
-	return 0;
+    return 0;
 }

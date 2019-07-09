34736d72c7ad4e3d9d04d02751a93a69ec3eb113
elkiss <jominhyuk87@gmail.com>
2019-07-09 22:55:28 +0900

    Updated ch18 thread4 source file.

--- tcpip/source/Linux/Tcpip18/thread4.c
+++ tcpip/source/Linux/Tcpip18/thread4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <windows.h>
-#include <process.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
 #define NUM_THREAD 100
 
 void* thread_inc(void* arg);
 void* thread_des(void* arg);
-
 long long num = 0;
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-    HANDLE thread_id[NUM_THREAD];
+    pthread_t thread_id[NUM_THREAD];
     int i;
 
     printf("sizeof long long: %d \n", sizeof(long long));
@@ -19,9 +19,9 @@ int main(int argc, char* argv[])
         if (i % 2) {
             pthread_create(&(thread_id[i]), NULL, thread_inc, NULL);
         } else {
-            pthread_create(&(thread_id[i]), NULL, thread_des, NULL);	
+            pthread_create(&(thread_id[i]), NULL, thread_des, NULL);
         }
-    }	
+    }
 
     for (i = 0; i < NUM_THREAD; ++i) {
         pthread_join(thread_id[i], NULL);
@@ -32,10 +32,9 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void * thread_inc(void* arg) 
+void* thread_inc(void* arg) 
 {
     int i;
-
     for (i = 0; i < 50000000; ++i) {
         num += 1;
     }
@@ -43,12 +42,11 @@ void * thread_inc(void* arg)
     return NULL;
 }
 
-void* thread_des(void* arg)
+void* thread_des(void* arg) 
 {
     int i;
-
     for (i = 0; i < 50000000; ++i) {
-        num-=1;
+        num -= 1;
     }
 
     return NULL;
